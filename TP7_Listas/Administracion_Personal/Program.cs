f1ec8ea07fb19e6e3ee1e7e04e9b7fbd11cac8e0
FacuCR <facucastro302@gmail.com>
2019-05-23 13:56:35 -0300

    Primer commit

---
+++ TP7_Listas/Administracion_Personal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administracion_Personal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        enum EstadoCivil { Casado, Soltero}
+        enum Genero { Masculino, Femenino}
+        enum Cargo { Auxiliar, Administrativo, Ingeniero, Especialista, Investigador}
+
+        public struct Fecha
+        {
+            int dia;
+            int mes;
+            int año;
+        }
+
+        public struct Empleado
+        {
+            string nombre;
+            string apellido;
+            Fecha fechaNacimiento;
+            EstadoCivil estadoCivil;
+            Genero genero;
+            Fecha fechaIngreso;
+            double sueldo;
+            Cargo cargo;
+        }
+    }
+}
