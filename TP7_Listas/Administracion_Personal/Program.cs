8f316cf21018d7d052332a94170809fa8e5ddc83
FacuCR <facucastro302@gmail.com>
2019-05-24 15:47:47 -0300

    TP Terminado

--- TP7_Listas/Administracion_Personal/Program.cs
+++ TP7_Listas/Administracion_Personal/Program.cs
@@ -11,64 +11,73 @@ namespace Administracion_Personal
         static void Main(string[] args)
         {
             Random random = new Random();
-            int ale = random.Next(4, 10);
+            //Valor aleatorio de empleados
+            int ale = random.Next(4, 20);
+            //Valores aleatorios para los enum
             int ale2;
-            Nombres nom;
-            Apellidos apel;
+
+            string nom = null;
+            string apel = null;
             DateTime fNac, fIng;
-            EstadoCivil eCivil;
-            Genero gen;
-            Cargo car;
-            DateTime fechaActual = new DateTime(2019, 5, 21);
+            string eCivil = null;
+            string gen = null;
+            string car = null;
+
+            DateTime fechaActual = DateTime.Now;
+            DateTime rangoFechaMax = new DateTime(1997, 1, 1);
             DateTime rangoMinFecha = new DateTime(1960, 1, 1);
-            int rangoFecha = (fechaActual - rangoMinFecha).Days;
-            //DateTime fechaActual = DateTime.Now();
-            //DateTime fechaActual = new DateTime(1999, 04, 24);
-            //DateTime fechaActual = DateTime(1995, 1, 1);
-            //DateTime FechaInicial = DateTime(1960, 1, 1);
-            //int rango = (fechaActual - fechaIncial).Days;
-            //Console.WriteLine("fecha : {0}", fechaActual.ToString());
-            //DateTime fechaAleatoria = fechaInicial.AddDays(rand.Next(rango));
+            int rangoFechaIng;
+            int rangoFecha = (rangoFechaMax - rangoMinFecha).Days;
 
             List<Empleado> empleados = new List<Empleado>();
             Empleado empleado = new Empleado();
             
-            //Cargar empleados en la lista
+            //Cargar una cantidad aleatoria de empleados en la lista
             for(int i = 0; i < ale; i++)
             {
                 //Seleccion aleatoria de nombres
                 ale2 = random.Next(9);
-                switch(ale2)
+                switch (ale2)
                 {
-                    case 0: nom = Nombres.Facundo;
-                    break;
+                    case 0:
+                        nom = Nombres.Facundo.ToString();
+                        break;
 
-                    case 1: nom = Nombres.Rafael;
-                    break;
+                    case 1:
+                        nom = Nombres.Rafael.ToString();
+                        break;
 
-                    case 2: nom = Nombres.Julieta;
-                    break;
+                    case 2:
+                        nom = Nombres.Julieta.ToString();
+                        break;
 
-                    case 3: nom = Nombres.Nicole;
-                    break;
+                    case 3:
+                        nom = Nombres.Nicole.ToString();
+                        break;
 
-                    case 4: nom = Nombres.Tomas;
-                    break;
+                    case 4:
+                        nom = Nombres.Tomas.ToString();
+                        break;
 
-                    case 5: nom = Nombres.Camila;
-                    break;
+                    case 5:
+                        nom = Nombres.Camila.ToString();
+                        break;
 
-                    case 6: nom = Nombres.Lionel;
-                    break;
+                    case 6:
+                        nom = Nombres.Lionel.ToString();
+                        break;
 
-                    case 7: nom = Nombres.Daniela;
-                    break;
+                    case 7:
+                        nom = Nombres.Daniela.ToString();
+                        break;
 
-                    case 8: nom = Nombres.Santiago;
-                    break;
+                    case 8:
+                        nom = Nombres.Santiago.ToString();
+                        break;
 
-                    case 9: nom = Nombres.Martina;
-                    break;
+                    case 9:
+                        nom = Nombres.Martina.ToString();
+                        break;
                 }
 
                 //Seleccion aleatoria de apellidos
@@ -76,43 +85,43 @@ namespace Administracion_Personal
                 switch (ale2)
                 {
                     case 0:
-                        apel = Apellidos.Castro;
+                        apel = Apellidos.Castro.ToString();
                         break;
 
                     case 1:
-                        apel = Apellidos.Rojo;
+                        apel = Apellidos.Rojo.ToString();
                         break;
 
                     case 2:
-                        apel = Apellidos.Aymar;
+                        apel = Apellidos.Aymar.ToString();
                         break;
 
                     case 3:
-                        apel = Apellidos.Stark;
+                        apel = Apellidos.Stark.ToString();
                         break;
 
                     case 4:
-                        apel = Apellidos.Gutierrez;
+                        apel = Apellidos.Gutierrez.ToString();
                         break;
 
                     case 5:
-                        apel = Apellidos.Perez;
+                        apel = Apellidos.Perez.ToString();
                         break;
 
                     case 6:
-                        apel = Apellidos.Acosta;
+                        apel = Apellidos.Acosta.ToString();
                         break;
 
                     case 7:
-                        apel = Apellidos.Ayala;
+                        apel = Apellidos.Ayala.ToString();
                         break;
 
                     case 8:
-                        apel = Apellidos.Albornoz;
+                        apel = Apellidos.Albornoz.ToString();
                         break;
 
                     case 9:
-                        apel = Apellidos.Reynoso;
+                        apel = Apellidos.Reynoso.ToString();
                         break;
                 }
 
@@ -124,10 +133,10 @@ namespace Administracion_Personal
                 switch(ale2)
                 {
                     case 0:
-                        eCivil = EstadoCivil.Casado;
+                        eCivil = EstadoCivil.Casado.ToString();
                         break;
                     case 1:
-                        eCivil = EstadoCivil.Soltero;
+                        eCivil = EstadoCivil.Soltero.ToString();
                         break;
                 }
 
@@ -136,40 +145,46 @@ namespace Administracion_Personal
                 switch(ale2)
                 {
                     case 0:
-                        gen = Genero.Masculino;
+                        gen = Genero.Masculino.ToString();
                         break;
                     case 1:
-                        gen = Genero.Femenino;
+                        gen = Genero.Femenino.ToString();
                         break;
                 }
 
                 //Fecha de ingreso
-                fIng = rangoMinFecha.AddDays(random.Next(rangoFecha));
+                rangoFechaIng = (fechaActual - fNac).Days;
+                fIng = fNac.AddDays(random.Next(rangoFechaIng));
 
                 //Cargo
                 ale2 = random.Next(4);
                 switch(ale2)
                 {
                     case 0:
-                        car = Cargo.Auxiliar;
+                        car = Cargo.Auxiliar.ToString();
                         break;
                     case 1:
-                        car = Cargo.Administrativo;
+                        car = Cargo.Administrativo.ToString();
                         break;
                     case 2:
-                        car = Cargo.Ingeniero;
+                        car = Cargo.Ingeniero.ToString();
                         break;
                     case 3:
-                        car = Cargo.Especialista;
+                        car = Cargo.Especialista.ToString();
                         break;
                     case 4:
-                        car = Cargo.Investigador;
+                        car = Cargo.Investigador.ToString();
                         break;
                 }
-
+                
+                //Creo un empleado
                 empleado.CargarEmpleado(nom, apel, fNac, eCivil, gen, fIng, car);
+
+                //Lo cargo en la lista
+                empleados.Add(empleado);
             }
-            //Menu(empleados);
+
+            Menu(empleados);
         }
 
         public enum Nombres { Facundo, Rafael, Julieta, Nicole, Tomas, Camila, Lionel, Daniela, Santiago, Martina}
@@ -182,50 +197,236 @@ namespace Administracion_Personal
 
         public struct Empleado
         {
-            Nombres nombre;
-            Apellidos apellido;
+            string nombre;
+            string apellido;
             DateTime fechaNacimiento;
-            EstadoCivil estadoCivil;
-            Genero genero;
+            string estadoCivil;
+            string genero;
             DateTime fechaIngreso;
             double sueldo;
-            Cargo cargo;
+            string cargo;
 
-            public void CargarEmpleado(Nombres _nombre, Apellidos _apellido, DateTime _fechaNacimiento, EstadoCivil _estadoCivil, Genero _genero, DateTime _fechaIngreso, Cargo _cargo)
+            public void CargarEmpleado(string _nombre, string _apellido, DateTime _fechaNacimiento, string _estadoCivil, string _genero, DateTime _fechaIngreso, string _cargo)
             {
+
+
                 nombre = _nombre;
                 apellido = _apellido;
                 fechaNacimiento = _fechaNacimiento;
                 estadoCivil = _estadoCivil;
                 genero = _genero;
                 fechaIngreso = _fechaIngreso;
-                sueldo = 12500;
+                sueldo = 15000;
                 cargo = _cargo;
             }
 
             public void MostrarEmpleado()
             {
+                Console.WriteLine("\n===============Datos del Empleado==============\n");
                 Console.WriteLine("Nombre: " + nombre);
                 Console.WriteLine("Apellido: " + apellido);
-                Console.WriteLine("Fecha de nacimiento: ");
+                Console.WriteLine("Fecha de nacimiento: " + fechaNacimiento);
                 Console.WriteLine("Genero: " + genero);
-                Console.WriteLine("Fecha de ingreso: ");
+                Console.WriteLine("Fecha de ingreso: " + fechaIngreso);
                 Console.WriteLine("Sueldo Basico: $" + sueldo);
                 Console.WriteLine("Cargo: " + cargo);
+                Console.WriteLine("\n===============================================\n");
+            }
+
+            public int CalcularAntiguedad()
+            {
+                DateTime fechaActual = DateTime.Today;
+
+                int antiguedad = (fechaActual.Year - fechaIngreso.Year);
+
+                return antiguedad;
+            }
+
+            public int CalcularEdad()
+            {
+                DateTime fechaActual = DateTime.Today;
+
+                int edad = (fechaActual.Year - fechaNacimiento.Year);
+
+                return edad;
+            }
+
+            public double CalcularAdicional()
+            {
+                Random random = new Random();
+                int hijos = random.Next(5);
+                int antiguedad = CalcularAntiguedad();
+                int cont = 0;
+                double adicional = 0, porcentajeSueldo = 0, porcentajeCargo = 0;
+
+                //Calcular el porcentaje del sueldo segun la antiguedad
+                if (antiguedad > 19)
+                {
+                    porcentajeSueldo = 0.25;
+                }
+                else
+                {
+                    while (cont < antiguedad)
+                    {
+                        porcentajeSueldo += 0.02;
+                        cont++;
+                    }
+                }
+
+                //Consultar si es casado y con 2 o mas hijos
+                bool casado = string.Equals(estadoCivil, EstadoCivil.Casado.ToString());
+
+                if(casado && hijos > 1)
+                {
+                    adicional += 5000;
+                }
+
+                adicional = adicional + (sueldo * porcentajeSueldo);
+
+                //Porcentaje segun el cargo
+                bool pComparacion = string.Equals(cargo, Cargo.Ingeniero.ToString());
+                bool sComparacion = string.Equals(cargo, Cargo.Especialista.ToString());
+
+                if (pComparacion || sComparacion)
+                {
+                    porcentajeCargo += 0.50;
+                    adicional = adicional + (adicional * porcentajeCargo);
+                }
+
+                return adicional;
+
+            }
+
+            public double CalcularSalario()
+            {
+                double salario = sueldo + CalcularAdicional();
+
+                return salario;
+            }
+
+            public int Jubilacion()
+            {
+                int jubilacion;
+                bool compGenero = string.Equals(genero, Genero.Masculino.ToString());
+
+                if(compGenero)
+                {
+                    return jubilacion = 65 - CalcularEdad();
+                }
+                else
+                {
+                    return jubilacion = 65 - CalcularEdad();
+                }
             }
         }
 
-        public static void Menu(Empleado empleados)
+        public static void Menu(List<Empleado> lista)
         {
-            int seleccion, ale;
+            int seleccion, seleccionEmp;
 
             do
             {
+                Console.WriteLine("\n.::==MENU==::.\n");
+
+                Console.WriteLine("\n1. Mostrar todos los empleados\n2. Seleccionar un empleado\n3. Mostrar salario total\n4. Cantidad total de empleados\n5. Salir");
+
                 seleccion = Convert.ToInt32(Console.ReadLine());
+
+                switch(seleccion)
+                {
+                    case 1:
+                        MostrarLista(lista);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Seleccione un empleado");
+
+                        seleccionEmp = Convert.ToInt32(Console.ReadLine());
+
+                        MenuEmpleado(lista[seleccionEmp]);
+
+                        break;
+
+                    case 3:
+                        SalarioTotal(lista);
+                        break;
+
+                    case 4:
+                        CantidadEmpleados(lista);
+                        break;
+                }
                 
 
             }
-            while (seleccion != 0);
+            while (seleccion != 5);
+        }
+
+        public static void MenuEmpleado(Empleado empleado)
+        {
+            int seleccion;
+
+            do
+            {
+                Console.WriteLine("\n.::==MENU DE EMPLEADO==::.\n");
+                Console.WriteLine("\n1. Antiguedad del empleado\n2. Edad del empleado\n3. Cuando falta para su jubilacion\n4. Salario\n5. Salir");
+
+                seleccion = Convert.ToInt16(Console.ReadLine());
+
+                switch(seleccion)
+                {
+                    case 1:
+                        Console.WriteLine("\nLa antiguedad del empleado es: {0} año(s)\n", empleado.CalcularAntiguedad());
+                        break;
+                    case 2:
+                        Console.WriteLine("\nLa edad del empleado es: {0} años\n", empleado.CalcularEdad());
+                        break;
+                    case 3:
+                        Console.WriteLine("\nAl empleado le faltan {0} años para jubilarse\n", empleado.Jubilacion());
+                        break;
+                    case 4:
+                        Console.WriteLine("\nEl salario del empleado es: ${0}\n", empleado.CalcularSalario());
+                        break;
+                }
+            }
+            while (seleccion != 5);
+        }
+
+        public static void MostrarLista(List<Empleado> lista)
+        {
+            int cont = 0;
+
+            foreach(Empleado elemento in lista)
+            {
+                Console.WriteLine("\n====Empleado {0}====\n", cont);
+
+                elemento.MostrarEmpleado();
+
+                cont++;
+            }
+        }
+
+        public static void SalarioTotal(List<Empleado> lista)
+        {
+            double salarioTotal = 0;
+
+            foreach(Empleado elemento in lista)
+            {
+                salarioTotal += elemento.CalcularSalario();
+            }
+
+            Console.WriteLine("\nMonto total a pagar: ${0}\n", salarioTotal);
+        }
+
+        public static void CantidadEmpleados(List<Empleado> lista)
+        {
+            int cont = 0;
+
+            foreach(Empleado elemento in lista)
+            {
+                cont++;
+            }
+
+            Console.WriteLine("\nLa cantidad total de empleados es: {0}\n", cont);
         }
     }
 }
